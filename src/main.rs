8b417f6cc41f351156e789da3f56100103f365cc
Zhenbang You <zhenbang_you@apple.com>
2023-07-05 19:33:58 -0700

    threaded

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use std::env;
+use std::fmt::Debug;
 use std::fs::{read_dir, File};
 use std::io::Read;
 use std::time::Instant;
-use string_builder::Builder;
+use std::{env, thread};
+
+const NUM_THREADS: usize = 8;
 
 fn main() {
     let now = Instant::now();
 
     let args: Vec<String> = env::args().collect();
-    let res = count_all_sub_files(&args[1], &args[2]);
+    let res = count_all_sub_files_threaded(&args[1], &args[2], NUM_THREADS);
 
     println!("{} ms", now.elapsed().as_millis());
 
@@ -76,105 +78,49 @@ fn count_lines(src: &str) -> usize {
     res
 }
 
-fn remove_comments_and_space(src: &str) -> String {
-    enum State {
-        Code,
-        SingleLineComment,
-        MultiLineComment,
-        Slash,
-        Asterisk,
-    }
-    let mut state = State::Code;
-    let mut builder = Builder::new(0);
-    for c in src.chars().into_iter() {
-        match state {
-            State::Code => {
-                if c != '/' {
-                    const SPACE_SET: [char; 3] = [' ', '\t', '\r'];
-                    if !SPACE_SET.contains(&c) {
-                        builder.append(c);
-                    }
-                } else {
-                    state = State::Slash;
-                }
-            }
-            State::SingleLineComment => {
-                if c == '\n' {
-                    state = State::Code;
-                }
-            }
-            State::MultiLineComment => {
-                if c == '*' {
-                    state = State::Asterisk;
-                }
-            }
-            State::Slash => {
-                if c == '/' {
-                    state = State::SingleLineComment;
-                } else if c == '*' {
-                    state = State::MultiLineComment;
-                } else {
-                    builder.append('/');
-                    builder.append(c);
-                    state = State::Code;
-                }
-            }
-            State::Asterisk => {
-                if c == '/' {
-                    state = State::Code;
-                } else if c != '*' {
-                    state = State::MultiLineComment;
-                }
-            }
-        }
-    }
-    builder.string().unwrap()
-}
-
-fn count_lines_str(src: &str) -> usize {
-    let src = remove_comments_and_space(src);
-    let src = src.trim_start();
-    fn count_double_endl(src: &str) -> usize {
-        let mut res = 0;
-        let mut is_prev_endl = false;
-        for c in src.chars() {
-            if c == '\n' && is_prev_endl {
-                res += 1;
-            }
-            is_prev_endl = c == '\n';
-        }
-        res
-    }
-    src.chars().filter(|c| *c == '\n').count() - count_double_endl(&src)
-}
-
 fn count_lines_file(path: &str) -> usize {
     let mut f = File::open(path).unwrap();
-    let mut buf = String::from("value");
+    let mut buf = String::from("");
     let _ = f.read_to_string(&mut buf).unwrap();
     count_lines(&buf)
 }
 
-fn count_all_sub_files(path: &str, suffix: &str) -> usize {
-    read_dir(path)
-        .unwrap()
-        .map(|f| {
-            let f = f.unwrap();
-            let file_type = f.file_type().unwrap();
-            let file_name = format!("{}/{}", path, f.file_name().to_str().unwrap());
-            if file_type.is_dir() {
-                count_all_sub_files(&file_name, suffix)
-            } else if file_type.is_file() {
-                if file_name.ends_with(suffix) {
-                    #[cfg(debug_assertions)]
-                    dbg!("{file_name}");
-                    count_lines_file(&file_name)
-                } else {
-                    0
-                }
-            } else {
-                0
+fn gather_all_sub_path(path: &str, suffix: &str, res: &mut Vec<String>) {
+    read_dir(path).unwrap().for_each(|f| {
+        let f = f.unwrap();
+        let file_type = f.file_type().unwrap();
+        let file_name = format!("{}/{}", path, f.file_name().to_str().unwrap());
+        if file_type.is_dir() {
+            gather_all_sub_path(&file_name, suffix, res);
+        } else if file_type.is_file() {
+            if file_name.ends_with(suffix) {
+                res.push(file_name);
             }
-        })
-        .fold(0, |a, b| a + b)
+        }
+    });
+}
+
+fn split_immut_vec<T: Debug>(v: &Vec<T>, num_slices: usize) -> Vec<Box<&[T]>> {
+    let num_slices = std::cmp::min(v.len(), num_slices);
+    let slice_len = (v.len() + num_slices - 1) / num_slices;
+    let mut res = vec![];
+    let mut remaining = &v[..];
+    while remaining.len()>0 {
+        let (head, tail) = remaining.split_at(std::cmp::min(slice_len,remaining.len()));
+        remaining = tail;
+        res.push(Box::new(head));
+    }
+    res
+}
+
+fn count_all_sub_files_threaded(path: &str, suffix: &str, num_slices: usize) -> usize {
+    let mut v = vec![];
+    gather_all_sub_path(path, suffix, &mut v);
+    let v = split_immut_vec(&v, num_slices);
+    thread::scope(|s| {
+        v.iter()
+            .map(|fs| s.spawn(|| fs.iter().map(|f| count_lines_file(f)).sum::<usize>()))
+            .map(|t| t.join().unwrap())
+            .sum()
+    })
 }
