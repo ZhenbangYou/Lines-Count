420e2503be3b6226e2f3980bac69b158a89a77b5
Zhenbang You <zhenbang_you@apple.com>
2023-07-06 11:36:18 -0700

    support multiple file name suffixes

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 
 example:
 
-```cargo run --release ../linux .c```
+```cargo run --release ../linux .c .h```
 
 The first argument specifies the directory of the project.
-The second argument specifies the file suffix we are concerned about.
-The above means counting the number of lines of code in files whose name ends with `.c` (i.e., all the C files) in directory (and all of its subdirectory) `../linux`.
+Other arguments specify the file suffix we are concerned about.
+The above means counting the number of lines of code in files whose name ends with `.c` and `.h` (i.e., all the C files and C header files) in directory (and all of its subdirectory) `../linux`.
 
 ## Note
 
-After parallelization, it becomes IO-bound, so setting the number of *jobs* (*NUM_JOBS*) to be more than the number of CPU cores (*NUM_CPU_CORES*) helps.
+After parallelization, it becomes IO-bound, so setting the number of *jobs* (*NUM_JOBS*) to be more than the number of CPU cores helps.
 The performance is very robust to *NUM_JOBS*, as long as it is not too small.
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::env;
 use std::fs::{read_dir, File};
 use std::io::Read;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Instant;
+use std::{env, thread};
 use threadpool::ThreadPool;
 use threadpool_scope::scope_with;
 
 const NUM_JOBS: usize = 1024;
-const NUM_CPU_CORES: usize = 10;
 fn main() {
     let now = Instant::now();
 
     let args: Vec<String> = env::args().collect();
-    let res = count_all_sub_files_threaded(&args[1], &args[2], NUM_JOBS);
+    let res = count_all_sub_files_threaded(&args[1], &args[2..], NUM_JOBS);
 
     println!("{} ms", now.elapsed().as_millis());
 
@@ -87,24 +86,24 @@ fn count_lines_file(path: &str) -> usize {
     count_lines(&buf)
 }
 
-fn gather_all_sub_path(path: &str, suffix: &str, res: &mut Vec<String>) {
+fn gather_all_sub_path(path: &str, suffixes: &[String], res: &mut Vec<String>) {
     read_dir(path).unwrap().for_each(|f| {
         let f = f.unwrap();
         let file_type = f.file_type().unwrap();
         let file_name = format!("{}/{}", path, f.file_name().to_str().unwrap());
         if file_type.is_dir() {
-            gather_all_sub_path(&file_name, suffix, res);
+            gather_all_sub_path(&file_name, suffixes, res);
         } else if file_type.is_file() {
-            if file_name.ends_with(suffix) {
+            if suffixes.iter().any(|s| file_name.ends_with(s)) {
                 res.push(file_name);
             }
         }
     });
 }
 
-fn count_all_sub_files_threaded(path: &str, suffix: &str, num_slices: usize) -> usize {
+fn count_all_sub_files_threaded(path: &str, suffixes: &[String], num_slices: usize) -> usize {
     let mut v = vec![];
-    gather_all_sub_path(path, suffix, &mut v);
+    gather_all_sub_path(path, suffixes, &mut v);
 
     let v = (0..num_slices)
         .map(|idx| {
@@ -117,7 +116,7 @@ fn count_all_sub_files_threaded(path: &str, suffix: &str, num_slices: usize) -> 
         .filter(|(a, b)| a < b)
         .map(|(a, b)| &v[a..b]);
 
-    let pool = ThreadPool::new(NUM_CPU_CORES);
+    let pool = ThreadPool::new(thread::available_parallelism().unwrap().into());
     let res = AtomicUsize::new(0);
     scope_with(&pool, |s| {
         v.for_each(|fs| {
